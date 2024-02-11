99e8e7a4239b2f4b0d96999c86872e884c238793
Merith <merith.tk@gmail.com>
2024-02-11 04:38:08 +0000

    apply some default launch args for managment

--- headless/.build/start_script.sh
+++ headless/.build/start_script.sh
@@ -1,4 +1,3 @@
-
 cd /data/home
 if [ -n $DISABLE_STEAMCMD ]; then
     echo "Running SteamCMD"
@@ -9,6 +8,8 @@ if [ -n $DISABLE_STEAMCMD ]; then
     /data/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} +force_install_dir /data/headless +app_update 2519830 -beta headless -betapassword ${STEAM_BETA} +quit
 fi
 
+DEFAULT_RESONITE_ARGS="-LogsPath /data/logs -DataPath /data/resonite/data -CachePath /data/resonite/cache"
+
 if [ ! -n $USE_CRYSTITE ]; then
     echo "Running Crystite"
     /usr/lib/crystite/crystite
@@ -16,11 +17,11 @@ else
     cd /data/headless/Headless
     if [ -n $RESONITE_MOD_LOADER ]; then
         echo "Running Resonite"
-        mono Resonite.exe
+        mono Resonite.exe $DEFAULT_RESONITE_ARGS $RESONITE_ARGS
     else
         echo "Running Resonite Mod Script"
         /mods_script.sh
         echo "Running Resonite with Mod Loader"
-        mono Resonite.exe -LoadAssembly Libraries/ResoniteModLoader.dll
+        mono Resonite.exe $DEFAULT_RESONITE_ARGS -LoadAssembly Libraries/ResoniteModLoader.dll $RESONITE_ARGS
     fi
 fi
